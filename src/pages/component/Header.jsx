aa20c9b23dfe1775a58498ecb21a7636525f4d6d
desperado1802 <mumun_ali@hotmail.com>
2023-02-21 14:08:22 +0200

    create the header component

---
+++ src/pages/component/Header.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+export default function Header() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  function pathMathRoute(route) {
+    if (route === location.pathname) {
+      return true;
+    }
+  }
+  return (
+    <div className="bg-white border-b shadow-sm sticky top-0 z-50">
+      <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
+        <div>
+          <img
+            onClick={() => navigate("/")}
+            src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg"
+            alt="logo"
+            className="h-5 cursor-pointer"
+          />
+        </div>
+        <div>
+          <ul className="flex space-x-10">
+            <li
+              onClick={() => navigate("/")}
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-transparent ${
+                pathMathRoute("/") && "text-current	  border-b-red-500"
+              }`}
+            >
+              Home
+            </li>
+            <li
+              onClick={() => navigate("/offers")}
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-transparent ${
+                pathMathRoute("/offers") && "text-current	  border-b-red-500"
+              }`}
+            >
+              Offers
+            </li>
+            <li
+              onClick={() => navigate("/sign-in")}
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-transparent ${
+                pathMathRoute("/sign-in") && "text-current	  border-b-red-500"
+              }`}
+            >
+              Sign in
+            </li>
+          </ul>
+        </div>
+      </header>
+    </div>
+  );
+}
