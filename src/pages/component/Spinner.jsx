09162cde5fdb82dbc6f8b763f5eff47f8166e51f
desperado1802 <mumun_ali@hotmail.com>
2023-02-23 16:02:54 +0200

    created the spinner component

---
+++ src/pages/component/Spinner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import spinner from "../../assets/svg/spinner.svg";
+
+export default function Spinner() {
+  return (
+    <div className="bg-black bg-opacity-50 flex items-center justify-center fixed left-0 right-0 bottom-0 top-0 z-50">
+      <div>
+        <img className="h-24" src={spinner} alt="Loading..." />
+      </div>
+    </div>
+  );
+}
