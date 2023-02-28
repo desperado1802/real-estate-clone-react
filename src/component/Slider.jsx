ae2929963b58f13f8cd0cac54112a2de17c76705
desperado1802 <mumun_ali@hotmail.com>
2023-02-28 19:28:02 +0200

    fixed the price on the slider so it has comma every 3 digits from right

--- src/component/Slider.jsx
+++ src/component/Slider.jsx
@@ -69,8 +69,14 @@ export default function Slider() {
                 {data.name}
               </p>
               <p className="text-[#f1faee] absolute left-1 bottom-1 font-semibold max-w-[90%] bg-[#e63946] shadow-lg opacity-90 p-2 rounded-tr-3xl  ">
-                {data.discountedPrice ?? data.regularPrice}$
-                {data.type === "rent" && " / month"}
+                {data.offer
+                  ? data.discounterPrice
+                      .toString()
+                      .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+                  : data.regularPrice
+                      .toString()
+                      .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                ${data.type === "rent" && " / month"}
               </p>
             </SwiperSlide>
           ))}
